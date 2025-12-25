907b2af3f9ce5a948c769657070800730846f60e
Md. Sourov Ahmed <96908951+MdSourovAhmed@users.noreply.github.com>
2025-12-25 22:29:13 +0600

    Update Url.js

--- Task1/Url-Shortner/backend/models/Url.js
+++ Task1/Url-Shortner/backend/models/Url.js
@@ -4,7 +4,6 @@ const urlSchema = new mongoose.Schema({
   shortCode: { type: String, required: true, unique: true },
   longUrl: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  // Optional: Add clicks or expiration if needed
 });
 
 module.exports = mongoose.model('Url', urlSchema);
